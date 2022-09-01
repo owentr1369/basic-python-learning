d4005ee8d22c00603ff6da9a33d7f217f8ede7af
TranTam <tamvaa1306@gmail.com>
2022-09-02 00:25:53 +0700

    variables

---
+++ variables/variables.py
@@ -0,0 +1,12 @@
+age = 20.5
+
+price = 19.95
+
+first_name = "Tran"
+
+last_name = "Tam"
+
+is_online: False
+
+print('Hello! My name is ' + first_name + " " + last_name)
+# print("I'm " + age + " years old!")
