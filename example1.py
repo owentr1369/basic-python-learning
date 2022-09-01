a7faf5714b765de95b8126bc56f53a82abdfa2fb
TranTam <tamvaa1306@gmail.com>
2022-09-02 00:13:18 +0700

    example2

--- example1.py
+++ example1.py
@@ -1,3 +1,3 @@
-name = input("Enter your name:: ")
+name = input("Enter your name: ")
 
-print("Hello" + name + "!")
+print("Hello " + name + "!")
---
+++ example2.py
@@ -0,0 +1,6 @@
+a = 100
+b = -255
+
+c = a+b
+
+print("Sum of a & b = ", c)
