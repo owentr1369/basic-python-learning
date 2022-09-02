de341594aaaece8287f2018e80c6bfe5eff94c9e
TranTam <tamvaa1306@gmail.com>
2022-09-02 15:31:26 +0700

    logical operator

---
+++ operators/operator-logical.py
@@ -0,0 +1,13 @@
+price1 = 25
+print(price1 > 10 and price1 < 30)
+# Output: True
+
+price2 = 5
+print(price2 > 10 or price2 < 30)
+# Output: True
+
+price3 = 5
+print(not price3 > 10)
+# Output: True
+
+# Logical operator: and, or, not
