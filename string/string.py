2cbc092d937ce76a937894fa35f5db5d0c34a951
TranTam <tamvaa1306@gmail.com>
2022-09-02 11:21:44 +0700

    strings

---
+++ string/string.py
@@ -0,0 +1,19 @@
+from dataclasses import replace
+from gettext import find
+from xmlrpc.client import Boolean
+
+
+course = 'Python for Beginners'
+
+# print(course.replace('for', '4'))
+print('Python' in course)
+# => Boolean
+
+# upper()
+# lower()
+# find()
+# replace()
+
+# => return new string object
+
+# print(course)
