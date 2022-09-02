59f9b758f785f780a6d5c05054433cb6c8490bd7
TranTam <tamvaa1306@gmail.com>
2022-09-03 00:00:00 +0700

    range function

---
+++ range-function/range-funciton.py
@@ -0,0 +1,22 @@
+# numbers = range(5)
+
+# print(numbers)
+# Output: range(0, 5)
+
+# for number in numbers:
+#     print(number)
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+numbers = range(5, 10, 2)
+
+for number in numbers:
+    print(number)
+# Output:
+# 5
+# 7
+# 9
