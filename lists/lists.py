a7982e0d5d1d8c99dca917eb99a7f02c5001a212
TranTam <tamvaa1306@gmail.com>
2022-09-02 16:58:12 +0700

    list

---
+++ lists/lists.py
@@ -0,0 +1,15 @@
+names = ['John', 'Jack', 'Bob', 'Alex']
+
+names[0] = 'Jon'
+
+print(names[2])
+# Output: Bob
+print(names[-1])
+# Output: Alex
+
+print(names[0])
+# Output: Jon
+
+print(names[0:3])
+# Output: ['Jon', 'Jack', 'Bob']
+# => Return new list
