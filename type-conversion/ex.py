5872baa7cc2025937b71f51222b3a3053cecee65
TranTam <tamvaa1306@gmail.com>
2022-09-02 10:27:41 +0700

    type conversion

---
+++ type-conversion/ex.py
@@ -0,0 +1,10 @@
+First = input("Enter first value: ")
+Second = input("Enter second value: ")
+
+Sum = float(First) + float(Second)
+
+print("Sum of First and Second value is: " + str(Sum))
+
+# Enter first value: 10
+# Enter second value: 10.1
+# Sum of First and Second value is: 20.1
---
+++ type-conversion/type-conversion.py
@@ -0,0 +1,11 @@
+birth_year = input('Enter your birth year: ')
+
+age = 2022 - int(birth_year)
+
+print(age)
+
+# float() 10.1
+
+# bool() Boolean
+
+# str() String
